8757a7e907e21e7f056af5fa10ebbeadfa461132
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-27 00:22:46 -0500

    289. Inicio de proyecto - ProductsApp

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+	root: true,
+	extends: '@react-native',
+	rules: {
+		'react-native/no-inline-styles': 0,
+		'prettier/prettier': 0,
+		'comma-dangle': 0,
+		'no-trailing-spaces': 0,
+		'@typescript-eslint/no-unused-vars': ['warn'],
+		'react/react-in-jsx-scope': 'off',
+		'react-hooks/exhaustive-deps': 'off',
+		curly: 'off',
+		'react/no-unstable-nested-components': 'off'
+	}
 };
