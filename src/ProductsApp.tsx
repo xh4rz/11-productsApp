5798a6a8f3b8993434596d1ab37d5d19194fc878
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-27 00:52:08 -0500

    291. Stack Navigator

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './presentation/navigation/StackNavigator';
 
 export const ProductsApp = () => {
 	return (
-		<View>
-			<Text>ProductsApp</Text>
-		</View>
+		<NavigationContainer>
+			<StackNavigator />
+		</NavigationContainer>
 	);
 };
---
+++ src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,31 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoadingScreen } from '../screens/loading/LoadingScreen';
+import { LoginScreen } from '../screens/auth/LoginScreen';
+import { RegisterScreen } from '../screens/auth/RegisterScreen';
+import { HomeScreen } from '../screens/home/HomeScreen';
+import { ProductScreen } from '../screens/product/ProductScreen';
+
+export type RootStackParams = {
+	LoadingScreen: undefined;
+	LoginScreen: undefined;
+	RegisterScreen: undefined;
+	HomeScreen: undefined;
+	ProductScreen: { productId: string };
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: true
+			}}>
+			<Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+			<Stack.Screen name="LoginScreen" component={LoginScreen} />
+			<Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+			<Stack.Screen name="HomeScreen" component={HomeScreen} />
+			<Stack.Screen name="ProductScreen" component={ProductScreen} />
+		</Stack.Navigator>
+	);
+};
