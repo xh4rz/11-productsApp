011c59dd7b1b31786a59de1a9828eabae7bb0adc
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-01-08 02:22:58 -0500

    310. Product - Interfaces y petición HTTP

---
+++ src/actions/auth/products/get-products-by-page.ts
@@ -0,0 +1,22 @@
+import { tesloApi } from '../../../config/api/tesloApi';
+import { TesloProduct } from '../../../infrastructure/interfaces/teslo-products.response';
+import { ProductMapper } from '../../../infrastructure/mappers/product.mapper';
+
+export const getProductsByPage = async (page: number, limit: number = 20) => {
+	console.log({ page, limit });
+	try {
+		const { data } = await tesloApi.get<TesloProduct[]>(
+			`/products?offset=${page * 10}&limit=${limit}`
+		);
+
+		const products = data.map(ProductMapper.tesloProductToEntity);
+
+		console.log(products[0]);
+
+		return products;
+	} catch (error) {
+		console.log(error);
+
+		throw new Error('Error getting products');
+	}
+};
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,8 +1,11 @@
 import { Button, Icon, Layout, Text } from '@ui-kitten/components';
 import { useAuthStore } from '../../store/auth/useAuthStore';
+import { getProductsByPage } from '../../../actions/auth/products/get-products-by-page';
 
 export const HomeScreen = () => {
 	const { logout } = useAuthStore();
+
+	getProductsByPage(0);
 
 	return (
 		<Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
