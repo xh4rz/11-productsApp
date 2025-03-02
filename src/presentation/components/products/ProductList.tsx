1586faae2c9b9f7e14eee2591daa06603602bf54
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-03-01 22:45:56 -0500

    316. useInfiniteQuery - InfiniteScroll de productos

--- src/presentation/components/products/ProductList.tsx
+++ src/presentation/components/products/ProductList.tsx
@@ -1,13 +1,25 @@
 import { Layout, List } from '@ui-kitten/components';
 import { Product } from '../../../domain/entities/product';
 import { ProductCard } from './ProductCard';
+import { useState } from 'react';
+import { RefreshControl } from 'react-native';
 
 interface Props {
 	products: Product[];
 	fetchNextPage: () => void;
 }
 
-export const ProductList = ({ products }: Props) => {
+export const ProductList = ({ products, fetchNextPage }: Props) => {
+	const [isRefreshing, setIsRefreshing] = useState(false);
+
+	const onPullToRefresh = async () => {
+		setIsRefreshing(true);
+
+		await new Promise(resolve => setTimeout(resolve, 1500));
+
+		setIsRefreshing(false);
+	};
+
 	return (
 		<List
 			data={products}
@@ -15,6 +27,11 @@ export const ProductList = ({ products }: Props) => {
 			keyExtractor={(item, index) => `${item.id}-${index}`}
 			renderItem={({ item }) => <ProductCard product={item} />}
 			ListFooterComponent={() => <Layout style={{ height: 150 }} />}
+			onEndReached={fetchNextPage}
+			onEndReachedThreshold={0.8}
+			refreshControl={
+				<RefreshControl refreshing={isRefreshing} onRefresh={onPullToRefresh} />
+			}
 		/>
 	);
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,25 +1,42 @@
 import { useAuthStore } from '../../store/auth/useAuthStore';
 import { getProductsByPage } from '../../../actions/auth/products/get-products-by-page';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { MainLayout } from '../../layouts/MainLayout';
 import { FullScreenLoader } from '../../components/ui/FullScreenLoader';
-import { Text } from '@ui-kitten/components';
 import { ProductList } from '../../components/products/ProductList';
 
 export const HomeScreen = () => {
 	const { logout } = useAuthStore();
 
-	const { isLoading, data: products = [] } = useQuery({
+	// const { isLoading, data: products = [] } = useQuery({
+	// 	queryKey: ['products', 'infinite'],
+	// 	staleTime: 1000 * 60 * 60, // 1 hour
+	// 	queryFn: () => getProductsByPage(0)
+	// });
+
+	const { isLoading, data, fetchNextPage } = useInfiniteQuery({
 		queryKey: ['products', 'infinite'],
 		staleTime: 1000 * 60 * 60, // 1 hour
-		queryFn: () => getProductsByPage(0)
+		initialPageParam: 0,
+		queryFn: async params => {
+			console.log({ params });
+			return await getProductsByPage(params.pageParam);
+		},
+		getNextPageParam: (lastPage, allPages) => allPages.length
 	});
 
 	return (
 		<MainLayout
 			title="TesloShop - Products"
 			subTitle="Aplicación administrativa">
-			{isLoading ? <FullScreenLoader /> : <ProductList products={products} />}
+			{isLoading ? (
+				<FullScreenLoader />
+			) : (
+				<ProductList
+					products={data?.pages.flat() ?? []}
+					fetchNextPage={fetchNextPage}
+				/>
+			)}
 		</MainLayout>
 	);
 };
