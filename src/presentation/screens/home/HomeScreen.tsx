ea03afd10a96ee0f6c69b1569032c42ab57be974
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-05-08 23:11:25 -0500

    325. Componente FAB

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import { getProductsByPage } from '../../../actions/auth/products/get-products-by-page';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { MainLayout } from '../../layouts/MainLayout';
 import { FullScreenLoader } from '../../components/ui/FullScreenLoader';
 import { ProductList } from '../../components/products/ProductList';
+import { Fab } from '../../components/ui/Fab';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../../navigation/StackNavigator';
 
 export const HomeScreen = () => {
+	const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
 	// const { isLoading, data: products = [] } = useQuery({
 	// 	queryKey: ['products', 'infinite'],
 	// 	staleTime: 1000 * 60 * 60, // 1 hour
@@ -20,17 +26,26 @@ export const HomeScreen = () => {
 	});
 
 	return (
-		<MainLayout
-			title="TesloShop - Products"
-			subTitle="Aplicación administrativa">
-			{isLoading ? (
-				<FullScreenLoader />
-			) : (
-				<ProductList
-					products={data?.pages.flat() ?? []}
-					fetchNextPage={fetchNextPage}
-				/>
-			)}
-		</MainLayout>
+		<>
+			<MainLayout
+				title="TesloShop - Products"
+				subTitle="Aplicación administrativa">
+				{isLoading ? (
+					<FullScreenLoader />
+				) : (
+					<ProductList
+						products={data?.pages.flat() ?? []}
+						fetchNextPage={fetchNextPage}
+					/>
+				)}
+			</MainLayout>
+			<Fab
+				iconName="plus-outline"
+				onPress={() =>
+					navigation.navigate('ProductScreen', { productId: 'new' })
+				}
+				style={{ position: 'absolute', bottom: 30, right: 20 }}
+			/>
+		</>
 	);
 };
