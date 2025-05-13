7b8b2d7922a8576c5876033a7c3c7a1164734f59
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-05-13 02:52:20 -0500

    336. Seleccionar de la galería de fotos

--- src/config/adapters/camera-adapter.ts
+++ src/config/adapters/camera-adapter.ts
@@ -1,4 +1,4 @@
-import { launchCamera } from 'react-native-image-picker';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 
 export class CameraAdapter {
 	static async takePicture(): Promise<string[]> {
@@ -16,6 +16,16 @@ export class CameraAdapter {
 	}
 
 	static async getPicturesFromLibrary(): Promise<string[]> {
+		const response = await launchImageLibrary({
+			mediaType: 'photo',
+			quality: 0.7,
+			selectionLimit: 10
+		});
+
+		if (response.assets && response.assets.length > 0) {
+			return response.assets.map(asset => asset.uri!);
+		}
+
 		return [];
 	}
 }
--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -58,7 +58,7 @@ export const ProductScreen = ({ route }: Props) => {
 					title={values.title}
 					subTitle={`Precio: ${values.price}`}
 					rightAction={async () => {
-						const photos = await CameraAdapter.takePicture();
+						const photos = await CameraAdapter.getPicturesFromLibrary();
 						setFieldValue('images', [...values.images, ...photos]);
 					}}
 					rightActionIcon="camera-outline">
